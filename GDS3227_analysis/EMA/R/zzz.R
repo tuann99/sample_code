de0e9c77b12ff35eac4d9c2d8173610a091509a4
Tuan Nguyen <113614022+tuann99@users.noreply.github.com>
2023-08-28 16:05:02 -0400

    Delete GDS3227_analysis directory

--- GDS3227_analysis/EMA/R/zzz.R
+++
@@ -1,9 +0,0 @@
-.onAttach <- function(lib, pkg){
-
-  packageStartupMessage("\n")
-  packageStartupMessage("################################################################################\n")
-  packageStartupMessage("Easy Microarray Analysis\n")
-  packageStartupMessage("EMA stable version\n")
-  packageStartupMessage("Current release : v1.4.4 - march 2014\n")
-  packageStartupMessage("################################################################################\n")    
-}
